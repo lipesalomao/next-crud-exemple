8ee6fe8570045f223e47d1d54decc11bac02ba67
Felipe Salomão <felipe.salomao.f@gmail.com>
2022-10-27 22:13:39 -0300

    :sparkles: feat: installation and configuration tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className={
+      `flex h-screen justify-center items-center`
+    }>
+      <span>Texto</span>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
